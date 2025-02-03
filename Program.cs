b7479809cb6975e742e83cc4a39e84e48081e832
Janat Kazibwe <janat.kazibwe@mnsu.edu>
2025-02-02 21:02:44 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class TriangleAreaCalculator
+{
+    static void Main()
+    {
+        //asking the user for the base
+        System.Console.Write("Enter the base of the triangle: ");
+        double baseSize = Convert.ToDouble(System.Console.ReadLine());
+
+        System.Console.Write("Enter the height of the triangle: ");
+        double height = Convert.ToDouble(System.Console.ReadLine());
+
+        //Calculate the area
+        double area = 0.5 * baseSize * height;
+
+        //output the result
+        System.Console.WriteLine($"The area of the triangle is: {area}");
+    }
+}
